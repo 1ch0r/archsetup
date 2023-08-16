4f5b808034f8e21fd5d48de5e4d8aa084b529fb7
1ch0r <95602383+1ch0r@users.noreply.github.com>
2023-08-16 03:07:06 -0500

    Update apps.sh

--- apps.sh
+++ apps.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ####################
-sudo pacman -S qtile lightdm lightdm-webkit2-greeter bluez bluez-utils networkmanager network-manager-applet nitrogen picom rofi htop neofetch kitty nano vim emacs wget git thunar lynis ufw arch-audit pwgen cracklib zsh zsh-syntax-highlighting neovim volumeicon virt-manager qemu-desktop dnsmasq iptables-nft ripgrep fs flameshot dunst exa lf khal bat bat-extras qtile-extras ntfs-3g os-prober curl python-psutils
+sudo pacman -S qtile lightdm lightdm-webkit2-greeter bluez bluez-utils networkmanager network-manager-applet nitrogen picom rofi htop neofetch kitty nano vim emacs wget git thunar lynis ufw arch-audit pwgen cracklib zsh zsh-syntax-highlighting neovim volumeicon virt-manager qemu-desktop dnsmasq iptables-nft ripgrep fd flameshot dunst exa lf khal bat bat-extras qtile-extras ntfs-3g os-prober curl python-psutils
 git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
 yay -S nerd-fonts-complete librewolf-bin
